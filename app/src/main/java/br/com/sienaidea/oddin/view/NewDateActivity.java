330684685edca1f9e5bbd46e0888ae1ac13890e7
Robson Monteiro <rrobsonmonteiro@gmail.com>
2016-09-27 10:30:38 -0300

    datas importantes

--- app/src/main/java/br/com/sienaidea/oddin/view/NewDateActivity.java
+++ app/src/main/java/br/com/sienaidea/oddin/view/NewDateActivity.java
@@ -1,5 +1,6 @@
 package br.com.sienaidea.oddin.view;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -10,10 +11,14 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 import br.com.sienaidea.oddin.R;
+import br.com.sienaidea.oddin.retrofitModel.Date;
 import br.com.sienaidea.oddin.retrofitModel.Instruction;
 import br.com.sienaidea.oddin.retrofitModel.Notice;
 import br.com.sienaidea.oddin.server.Preference;
@@ -24,11 +29,13 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NewDateActivity extends AppCompatActivity {
-    private TextInputLayout mTextInputLayoutSubject, mTextInputLayoutNotice;
-    private EditText mEditTextSubject, mEditTextNotice;
+    private TextInputLayout mTextInputLayoutDate, mTextInputLayoutSubject, mTextInputLayoutDescription;
+    private EditText mEditTextDate, mEditTextSubject, mEditTextDescription;
     private View mRootLayout;
     private Instruction mInstruction;
-    private Notice mNotice;
+    private Date mDate;
+    private int mYear, mMonth, mDay;
+    private Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +43,18 @@ public class NewDateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_date);
 
         mRootLayout = findViewById(R.id.root);
+        mTextInputLayoutDate = (TextInputLayout) findViewById(R.id.til_date);
         mTextInputLayoutSubject = (TextInputLayout) findViewById(R.id.til_subject);
-        mTextInputLayoutNotice = (TextInputLayout) findViewById(R.id.til_notice);
+        mTextInputLayoutDescription = (TextInputLayout) findViewById(R.id.til_description);
+        mEditTextDate = (EditText) findViewById(R.id.et_date);
         mEditTextSubject = (EditText) findViewById(R.id.et_subject);
-        mEditTextNotice = (EditText) findViewById(R.id.et_notice);
+        mEditTextDescription = (EditText) findViewById(R.id.et_description);
 
         if (savedInstanceState != null) {
             mInstruction = savedInstanceState.getParcelable(Instruction.TAG);
+            mEditTextDate.setText(savedInstanceState.getString("mEditTextDate"));
             mEditTextSubject.setText(savedInstanceState.getString("mEditTextSubject"));
-            mEditTextNotice.setText(savedInstanceState.getString("mEditTextNotice"));
+            mEditTextDescription.setText(savedInstanceState.getString("mEditTextDescription"));
         } else {
             if (getIntent().getExtras() != null) {
                 mInstruction = getIntent().getParcelableExtra(Instruction.TAG);
@@ -63,32 +73,60 @@ public class NewDateActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        initDatePicker();
     }
 
-    private void newNotice() {
+    private void initDatePicker() {
+        // Get Current Date
+        myCalendar = Calendar.getInstance();
+        mYear = myCalendar.get(Calendar.YEAR);
+        mMonth = myCalendar.get(Calendar.MONTH);
+        mDay = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        mEditTextDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+
+        mEditTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    private void newDate() {
         if (!validate()) {
             return;
         } else {
             DetectConnection mDetectConnection = new DetectConnection(getApplicationContext());
             if (mDetectConnection.existConnection()) {
-                mNotice = new Notice();
-                mNotice.setSubject(mEditTextSubject.getText().toString());
-                mNotice.setText(mEditTextNotice.getText().toString());
+                mDate = new Date();
+                mDate.setDate(mEditTextDate.getText().toString());
+                mDate.setSubject(mEditTextSubject.getText().toString());
+                mDate.setText(mEditTextDescription.getText().toString());
 
                 Preference preference = new Preference();
 
-                Call<Notice> request = Retrofit.getInstance().createInstructionNotices(preference.getToken(getApplicationContext()), mInstruction.getLecture().getId(), mNotice);
-                request.enqueue(new Callback<Notice>() {
+                Call<Date> request = Retrofit.getInstance().createInstructionDate(preference.getToken(getApplicationContext()), mInstruction.getLecture().getId(), mDate);
+                request.enqueue(new Callback<Date>() {
                     @Override
-                    public void onResponse(Call<Notice> call, Response<Notice> response) {
+                    public void onResponse(Call<Date> call, Response<Date> response) {
                         if (response.isSuccessful()) {
-                            mNotice = response.body();
+                            mDate = response.body();
                             onRequestSuccess();
                         } else onRequestFailure();
                     }
 
                     @Override
-                    public void onFailure(Call<Notice> call, Throwable t) {
+                    public void onFailure(Call<Date> call, Throwable t) {
                         onRequestFailure();
                     }
                 });
@@ -98,7 +136,7 @@ public class NewDateActivity extends AppCompatActivity {
                         .setAction(R.string.snake_try_again, new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                newNotice();
+                                newDate();
                             }
                         }).show();
             }
@@ -108,6 +146,13 @@ public class NewDateActivity extends AppCompatActivity {
     private boolean validate() {
         boolean valid = true;
 
+        if (TextUtils.isEmpty(mEditTextDate.getText().toString().trim())) {
+            mTextInputLayoutDate.setError(getResources().getString(R.string.error_field_required));
+            valid = false;
+        } else {
+            mTextInputLayoutDate.setError(null);
+        }
+
         if (TextUtils.isEmpty(mEditTextSubject.getText().toString().trim())) {
             mTextInputLayoutSubject.setError(getResources().getString(R.string.error_field_required));
             valid = false;
@@ -115,18 +160,18 @@ public class NewDateActivity extends AppCompatActivity {
             mTextInputLayoutSubject.setError(null);
         }
 
-        if (TextUtils.isEmpty(mEditTextNotice.getText().toString().trim())) {
-            mTextInputLayoutNotice.setError(getResources().getString(R.string.error_field_required));
+        if (TextUtils.isEmpty(mEditTextDescription.getText().toString().trim())) {
+            mTextInputLayoutDescription.setError(getResources().getString(R.string.error_field_required));
             valid = false;
         } else {
-            mTextInputLayoutNotice.setError(null);
+            mTextInputLayoutDescription.setError(null);
         }
         return valid;
     }
 
     private void onRequestSuccess() {
         Intent intentResult = new Intent();
-        intentResult.putExtra(Notice.TAG, mNotice);
+        intentResult.putExtra(Date.TAG, mDate);
         setResult(RESULT_OK, intentResult);
         finish();
     }
@@ -149,7 +194,7 @@ public class NewDateActivity extends AppCompatActivity {
             finish();
         }
         if (id == R.id.action_send) {
-            newNotice();
+            newDate();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -158,8 +203,9 @@ public class NewDateActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putParcelable(Instruction.TAG, mInstruction);
-        outState.putString("mEditTextSubject", mEditTextSubject.getText().toString());
-        outState.putString("mEditTextNotice", mEditTextNotice.getText().toString());
+        outState.putString("mEditTextDate", mEditTextDate.getText().toString());
+        outState.putString("mEditTextDescription", mEditTextDescription.getText().toString());
+        outState.putString("mEditTextDescription", mEditTextDescription.getText().toString());
         super.onSaveInstanceState(outState);
     }
 }
